f7d12d57f1c074d9cb23b32b8e63d4ac1cca5411
Coke-GalPi <scoutkoke@gmail.com>
2022-12-15 20:04:18 -0300

    Intento numero ? para ralizar conexión a bbdd

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, File
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from model.db_connection import Connection
+
 
 app = FastAPI()
 conn = Connection()
 
 template = Jinja2Templates(directory="./view")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse) # Index
 def root(req: Request):
     return template.TemplateResponse("index.html", {"request": req})
 
-@app.get("/signin", response_class=HTMLResponse)
+@app.get("/signin", response_class=HTMLResponse) # Login
 def signin(req: Request):
-    conn
     return template.TemplateResponse("signin.html", {"request": req})
 
-@app.get("/signup", response_class=HTMLResponse)
+@app.get("/signup", response_class=HTMLResponse) # Registro de Usuario
 def signup(req: Request):
-    conn
     return template.TemplateResponse("signup.html", {"request": req})
+
+@app.get("/contact", response_class=HTMLResponse) # Perfil de Usuario
+def user(req: Request):
+    return template.TemplateResponse("contact.html", {"request": req})
